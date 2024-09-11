c9c983807c18c81856652faf7ba3f8aa3db8145c
dorgui00 <dorian.guillon@edu.devinci.fr>
2024-09-11 14:41:03 +0200

    add question

---
+++ Assets/Sprites/Question.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Question", menuName = "question", order = 0)]
+
+public class Question : ScriptableObject
+{
+
+    public List<QuestionData> datas = new();
+
+
+    public void QuestionData()
+    {
+       
+
+       
+    }
+
+
+    
+}
---
+++ Assets/Sprites/questionData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using static QuestionData;
+
+[Serializable]
+
+public class QuestionData
+{
+    public string question;
+   
+    public int reponce;
+}
